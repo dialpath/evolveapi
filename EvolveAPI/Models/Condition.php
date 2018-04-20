96f6ed492987d19f0f0d5fab3c72294dddd82d53
= <chris@vocalogic.com>
2018-04-20 14:59:54 -0400

    Condition Model

---
+++ EvolveAPI/Models/Condition.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 4/20/18
+ * Time: 2:47 PM
+ */
+
+namespace EvolveAPI\Models;
+
+
+use EvolveAPI\EVCore;
+
+class Condition extends EVCore
+{
+    /**
+     * Carrier constructor.
+     * @param string $environment
+     */
+    public function __construct(string $environment)
+    {
+        parent::__construct();
+        $this->environment = $environment;
+    }
+
+    /**
+     * Get a list of Conditions currently in the PBX.
+     * @param string $pbx
+     * @return Conditions
+     * @throws \EvolveAPI\EVException
+     */
+    public function index(string $pbx)
+    {
+        return $this->send("pbx/{$pbx}/conditions")->conditions;
+    }
+
+    /**
+     * Get a single condition
+     * @param string $pbx
+     * @param string $uuid
+     * @return Condition
+     * @throws \EvolveAPI\EVException
+     */
+    public function find(string $pbx, string $uuid)
+    {
+        return $this->send("pbx/{$pbx}/conditions/{$uuid}")->condition;
+    }
+
+    /**
+     * Create a new Condition and return a UUID
+     * @param string $pbx
+     * @param array $params
+     * @return string
+     * @throws \EvolveAPI\EVException
+     */
+    public function create(string $pbx, array $params = []) : string
+    {
+        return $this->send("pbx/{$pbx}/conditions", 'POST', $params)->uuid;
+    }
+
+    /**
+     * Update a condition
+     * @param string $pbx
+     * @param $uuid
+     * @param array $params
+     * @return string
+     * @throws \EvolveAPI\EVException
+     */
+    public function update(string $pbx, $uuid, array $params = []) : string
+    {
+        return $this->send("pbx/{$pbx}/conditions/{$uuid}", 'PUT', $params)->uuid;
+    }
+
+    /**
+     * Delete a Condition
+     * @param string $pbx
+     * @param $uuid
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function destroy(string $pbx, $uuid)
+    {
+        return $this->send("pbx/{$pbx}/conditions/{$uuid}", 'DELETE');
+    }
+
+}
