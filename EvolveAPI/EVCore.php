63450b853c47de723d6e43426acd75fbb36d465a
youssef sanad <yousef_sanad@hotmail.com>
2018-06-25 22:54:08 +0200

    changing baseEndPoint

--- EvolveAPI/EVCore.php
+++ EvolveAPI/EVCore.php
@@ -28,7 +28,7 @@ class EVCore
      * The Evolve base endpoint. This hopefully won't ever change.
      * @var string
      */
-    private $baseEndpoint = "http://localhost/api/v1/";
+    private $baseEndpoint = "http://dialpath.test/api/v1/";
     /**
      * Our Guzzle Client
      * @var
