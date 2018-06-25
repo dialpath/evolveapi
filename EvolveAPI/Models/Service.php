2ed8fed67e9cc1fb87ff546d4546f39658837d1f
youssef sanad <yousef_sanad@hotmail.com>
2018-06-25 20:48:46 +0200

    Service Model is now created

---
+++ EvolveAPI/Models/Service.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cdc
+ * Date: 25-Jun-18
+ * Time: 4:55 PM
+ */
+
+namespace EvolveAPI\Models;
+
+use EvolveAPI\EVCore;
+
+class Service extends EVCore
+{
+    /**
+     * Order constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get a list of services.
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function all()
+    {
+        return $this->send("services")->services;
+    }
+}
