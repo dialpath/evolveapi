85ed959bda0ee0aada86ff17877de60e3d33ec94
youssef sanad <yousef_sanad@hotmail.com>
2018-06-27 18:02:22 +0200

    added VoiceMailMessage Model

---
+++ EvolveAPI/Models/VoiceMailMessage.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cdc
+ * Date: 27-Jun-18
+ * Time: 5:46 PM
+ */
+
+namespace EvolveAPI\Models;
+
+
+class VoiceMailMessage extends EVCore
+{
+    /**
+     * VoiceMailMessage constructor.
+     * @param string $environment
+     */
+    public function __construct(string $environment)
+    {
+        parent::__construct();
+        $this->environment = $environment;
+    }
+
+    /**
+     * @param string $pbx
+     * @param bool $includeExtensions
+     * @return mixed
+     */
+    public function all(string $pbx, string $vm, $includeExtensions = false)
+    {
+        $withExtensions = $includeExtensions ? ['withExtensions' => true] : [];
+        return $this->send("pbx/{$pbx}/voicemails/{$vm}/messages", 'GET', $withExtensions)->messages;
+    }
+}
