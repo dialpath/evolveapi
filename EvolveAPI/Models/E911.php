34bda8a8fb9082299a19f1d1df5b763ace367a81
Chris Horne <chris@vocalogic.com>
2018-07-02 15:13:59 -0400

    E911 Updates

--- EvolveAPI/Models/E911.php
+++ EvolveAPI/Models/E911.php
@@ -14,22 +14,37 @@ use EvolveAPI\EVCore;
 class E911 extends EVCore
 {
     /**
-     * Get a list of all E911 locations currently stored in DialPath
+     * Extension constructor.
+     * @param string $environment
      */
-    public function all()
+    public function __construct(string $environment)
     {
-        return $this->send("e911")->locations;
+        parent::__construct();
+        $this->environment = $environment;
+    }
+
+    /**
+     * Get a list of all E911 locations currently stored in DialPath
+     * @param string $pbx Specify a UUID of a PBX or use 'all' to get all
+     * locations on the account.
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function all(string $pbx = 'all')
+    {
+        return $this->send("pbx/{$pbx}/e911")->locations;
     }
 
     /**
      * Get a single E911 Location
+     * @param string $pbx
      * @param string $uuid
      * @return mixed
      * @throws \EvolveAPI\EVException
      */
-    public function find(string $uuid)
+    public function find(string $pbx, string $uuid)
     {
-        return $this->send("e911/{$uuid}")->location;
+        return $this->send("pbx/{$pbx}/e911/{$uuid}")->location;
     }
 
     /**
@@ -37,39 +52,42 @@ class E911 extends EVCore
      * you can assign telephone numbers on your account to those locations.
      * You can lookup valid E911 addresses using the USPS lookup tool.
      * @reference https://tools.usps.com/zip-code-lookup.htm?byaddress
+     * @param string $pbx
      * @param array $locationData
      * @return mixed
      * @throws \EvolveAPI\EVException
      */
-    public function create(array $locationData)
+    public function create(string $pbx, array $locationData)
     {
-        return $this->send("e911", 'post', ['location' => $locationData]);
+        return $this->send("pbx/{$pbx}/e911", 'post', ['location' => $locationData]);
     }
 
     /**
      * Update an existing location.
      * NOTE: This will change the e911 Location information for all numbers currently assigned
      * to this location.
-     * @param $uuid
+     * @param string $pbx
+     * @param string $uuid
      * @param array $locationData
      * @return mixed
      * @throws \EvolveAPI\EVException
      */
-    public function update($uuid, array $locationData)
+    public function update(string $pbx, string $uuid, array $locationData)
     {
-        return $this->send("e911/{$uuid}", 'post', ['location' => $locationData]);
+        return $this->send("pbx/{$pbx}/e911/{$uuid}", 'post', ['location' => $locationData]);
     }
 
     /**
      * Remove an E911 location. This will remove any telephone number associations
      * to this address. Make sure you update your numbers accordingly.
+     * @param string $pbx
      * @param string $uuid
      * @return mixed
      * @throws \EvolveAPI\EVException
      */
-    public function delete(string $uuid)
+    public function delete(string $pbx, string $uuid)
     {
-        return $this->send("e911/{$uuid}", 'DELETE');
+        return $this->send("pbx/{$pbx}/e911/{$uuid}", 'DELETE');
     }
 
 }
