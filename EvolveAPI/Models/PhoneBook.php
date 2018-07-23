7ed3cb63525434e1f9791dfd4851b1b074692f0a
youssef sanad <yousef_sanad@hotmail.com>
2018-07-23 19:14:51 +0200

    added the whole crud funcitions for the PhoneBook model

--- EvolveAPI/Models/PhoneBook.php
+++ EvolveAPI/Models/PhoneBook.php
@@ -33,4 +33,53 @@ class PhoneBook extends EVCore
     {
         return $this->send("pbx/{$pbx}/phonebooks", 'GET', ['pbx' => $pbx])->phoneBooks;
     }
+
+    /**
+     * Create a new PhoneBook record
+     * @param $pbx
+     * @param array $params
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function create($pbx, array $params)
+    {
+        return $this->send("pbx/{$pbx}/phonebooks", 'POST', $params);
+    }
+
+
+    /**
+     * @param $pbx
+     * @param $uuid
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function find($pbx, $uuid)
+    {
+        return $this->send("pbx/{$pbx}/phonebooks/{$uuid}")->number;
+    }
+
+
+    /**
+     * @param $pbx
+     * @param $uuid
+     * @param array $params
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function update($pbx, $uuid, array $params)
+    {
+        return $this->send("pbx/{$pbx}/phonebooks/$uuid", 'PUT', $params);
+    }
+
+
+    /**
+     * @param $pbx
+     * @param $uuid
+     * @return mixed
+     * @throws \EvolveAPI\EVException
+     */
+    public function delete($pbx, $uuid)
+    {
+        return $this->send("pbx/{$pbx}/phonebooks/$uuid", 'DELETE');
+    }
 }
