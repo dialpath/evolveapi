bdbd788cae66eb89957e3aa99860b44c77a3d1b5
youssef sanad <yousef_sanad@hotmail.com>
2018-07-26 14:03:54 +0200

    Created the EmailTemplate Model

---
+++ EvolveAPI/Models/EmailTemplate.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cdc
+ * Date: 26-Jul-18
+ * Time: 1:57 PM
+ */
+
+namespace EvolveAPI\Models;
+
+
+use EvolveAPI\EVCore;
+
+class EmailTemplate extends EVCore
+{
+    /**
+     * Email Template constructor.
+     * @param string $environment
+     */
+    public function __construct(string $environment)
+    {
+        parent::__construct();
+        $this->environment = $environment;
+    }
+
+    /**
+     * @param string $pbx
+     * @param string $type
+     * @return EmailTemplate
+     * @throws \EvolveAPI\EVException
+     */
+    public function showTemplate(string $pbx, string $type)
+    {
+        return $this->send("pbx/{$pbx}/templates/{$type}")->template;
+    }
+
+    /**
+     * @param string $pbx
+     * @param string $type
+     * @param array $params
+     * @return string
+     * @throws \EvolveAPI\EVException
+     */
+    public function updateTemplate(string $pbx, string $type, array $params = [])
+    {
+        return $this->send("pbx/{$pbx}/templates/{$type}", 'PUT', $params)->uuid;
+    }
+}
